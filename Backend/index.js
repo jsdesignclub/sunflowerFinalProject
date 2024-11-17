300adc46d7f05b03aaffc1992e58b511d1741e1d
jsdesignclub <123974954+jsdesignclub@users.noreply.github.com>
2024-11-17 22:14:19 +0530

    final comitt

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes'); // Import user routes
-const bcrypt = require('bcryptjs');
+//const bcrypt = require('bcryptjs');
 
 const app = express();
 const port = process.env.PORT || 5000;
